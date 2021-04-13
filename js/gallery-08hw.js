0eccf0b035721d79cf9d6ac55646caba653eba75
Tatyana Tupalo <tatyana080489t@gmail.com>
2021-04-13 12:51:06 +0300

    в поисках src

--- js/gallery-08hw.js
+++ js/gallery-08hw.js
@@ -1,63 +1,46 @@
 import galleryItems from "./gallery-items.js";
-
 const galleryList = document.querySelector(".js-gallery");
 const lightbox = document.querySelector(".lightbox");
 const btn = document.querySelector('button[data-action="close-lightbox"]');
 console.log(btn);
 const modal = document.querySelector(".lightbox__content");
 const lightboxImage = document.querySelector(".lightbox__image");
-
 const makeGallery = ({ preview, original, description }) => {
   return `<li class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" src=${preview} data-source=${original}
       alt=${description}/></a>
     </li>`;
 };
-
 const markup = galleryItems.map(makeGallery).join("");
 console.log(markup);
 galleryList.insertAdjacentHTML("afterbegin", markup);
-
 galleryList.addEventListener("click", openGallery);
 modal.addEventListener("click", closeModal);
 btn.addEventListener("click", onBtnClick);
-
 function openGallery(e) {
   e.preventDefault();
-
   if (e.target.nodeName !== "IMG") {
     return;
   }
   if (e.target.nodeName === "IMG") {
     lightbox.classList.add("is-open");
-    onClicklightboxImage();
-    // lightboxImage.src = e.target.getAttribute("data-source");
-    // lightboxImage.alt = e.target.alt;
+    onClicklightboxImage(e); 
   }
   window.addEventListener("keydown", clickKey);
 }
-
 function closeModal(e) {
   lightbox.classList.remove("is-open");
-  onClicklightboxImage("", "");
- 
-  // lightboxImage.src = "";
-  // lightboxImage.alt = "";
-}
-
-function onClicklightboxImage(e) {
-  const showImage = lightboxImage(src, alt);
-  lightboxImage.src = e.target.dataset.source;
+  onClicklightboxImage(e);
   
+}
+function onClicklightboxImage(e) {
+  lightboxImage.src = e.target.dataset.source;
   lightboxImage.alt = e.target.alt;
 }
-
-  
 function onBtnClick(e) {
   if (e.target === e.currentTarget) {
     closeModal();
   }
 }
-
 function clickKey(e) {
   if (e.code === "Escape") {
     closeModal();
